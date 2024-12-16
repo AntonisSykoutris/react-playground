8ff38803c76b6ff7ac1bd875d2024767f07f2201
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-12-16 21:39:03 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return <div className='mx-12 flex h-screen w-screen flex-col  container gap-y-10   p-0 pt-0'>Hello</div>;
+};
+
+export default App;
